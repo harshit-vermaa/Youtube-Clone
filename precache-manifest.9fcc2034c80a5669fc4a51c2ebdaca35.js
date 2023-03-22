620800e23720892d440f269abbbbd5e764b39fb0
harshit-vermaa <harshitvermaoffical59@gmail.com>
2023-03-22 17:16:31 +0530

    Updates

---
+++ precache-manifest.9fcc2034c80a5669fc4a51c2ebdaca35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "245834700688df126214",
+    "url": "/Youtube-Clone/static/css/main.e5ebaec5.chunk.css"
+  },
+  {
+    "revision": "245834700688df126214",
+    "url": "/Youtube-Clone/static/js/main.24583470.chunk.js"
+  },
+  {
+    "revision": "49f4a745e9a43a153cbd",
+    "url": "/Youtube-Clone/static/js/1.49f4a745.chunk.js"
+  },
+  {
+    "revision": "075aaeab3ba8d63711ce",
+    "url": "/Youtube-Clone/static/js/2.075aaeab.chunk.js"
+  },
+  {
+    "revision": "1a6f58b5e92b5b949e4e",
+    "url": "/Youtube-Clone/static/js/runtime~main.1a6f58b5.js"
+  },
+  {
+    "revision": "de35b6ed87a890a654980d3b2ff1432d",
+    "url": "/Youtube-Clone/index.html"
+  }
+];
